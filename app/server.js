14306d631e3c8813a7e2eb723243c44fd9da4127
borisding <boris.ding82@outlook.com>
2021-07-13 16:42:58 +0800

    configure eta view engine

--- app/server.js
+++ app/server.js
@@ -1,11 +1,20 @@
 import express from 'express'
 import dotenv from 'dotenv';
 import chalk from 'chalk'
+import * as eta from 'eta';
 
-dotenv.config();
-
+import { isDev, paths } from './utils.js';
 const app = express()
 
+// load node environment variables
+dotenv.config();
+
+// app view engine and directories config
+eta.configure({ cache: !isDev });
+app.engine('eta', eta.renderFile);
+app.set('view engine', 'eta');
+app.set('views', [paths.views]);
+  
 // running express app server
 const PORT = parseInt(process.env.PORT, 10) ?? 5000;
 app.listen(PORT, () => {
--- app/utils.js
+++ app/utils.js
@@ -1,1 +1,11 @@
+import path from 'path';
+
 export const isDev = process.env.NODE_ENV === 'development'
+
+export const paths = {
+    controlles: path.resolve('controlles'),
+    middleware: path.resolve('middleware'),
+    models: path.resolve('models'),
+    routers: path.resolve('routers'),
+    views: path.resolve('views')
+}
