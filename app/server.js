6d9ec2c342bb11370eeeeda7bcfc122bd7c0bb88
borisding <boris.ding82@outlook.com>
2021-07-14 22:53:42 +0800

    import tweak

--- app/server.js
+++ app/server.js
@@ -8,13 +8,13 @@ import csurf from 'csurf';
 import favicon from 'serve-favicon';
 import * as eta from 'eta';
 
-import * as routers from './routers/index.js';
 import {
   httpLogger,
   csrfToken,
   notFound,
   errorHandler
 } from './middleware/index.js';
+import * as routers from './routers/index.js';
 import { env, paths } from '../utils/index.js';
 
 const app = express();
