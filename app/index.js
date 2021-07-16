fe03282588c93d2cd73ee00aa24e12614b613e2b
borisding <boris.ding82@outlook.com>
2021-07-16 11:14:51 +0800

    move app to index.js. handle server shutdown gracefully

---
+++ app/index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import favicon from 'serve-favicon';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import csurf from 'csurf';
+import * as eta from 'eta';
+
+import * as middleware from './middleware/index.js';
+import * as routers from './routers/index.js';
+import { env, paths } from '../utils/index.js';
+
+const app = express();
+
+// app view engine and directory config
+eta.configure({ cache: env.isProd });
+app
+  .engine('eta', eta.renderFile)
+  .set('view engine', 'eta')
+  .set('views', `${paths.assets}/views`);
+
+// app middleware
+app
+  .use(helmet())
+  .use(middleware.httpLogger())
+  .use(cookieParser())
+  .use(compression())
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }), hpp())
+  .use(express.static(paths.public))
+  .use(favicon(`${paths.public}/favicon.ico`))
+  .use(csurf({ cookie: true }), middleware.csrfToken());
+
+// app routes
+app.use('/', routers.home);
+
+// app error handlers
+app.use(middleware.notFound());
+app.use(middleware.errorHandler());
+
+export default app;
--- app/server.js
+++ app/server.js
@@ -1,52 +1,36 @@
-import express from 'express';
 import chalk from 'chalk';
-import helmet from 'helmet';
-import hpp from 'hpp';
-import favicon from 'serve-favicon';
-import compression from 'compression';
-import cookieParser from 'cookie-parser';
-import csurf from 'csurf';
-import * as eta from 'eta';
-
-import * as middleware from './middleware/index.js';
-import * as routers from './routers/index.js';
-import { env, paths } from '../utils/index.js';
-
-const app = express();
-
-// app view engine and directory config
-eta.configure({ cache: env.isProd });
-app
-  .engine('eta', eta.renderFile)
-  .set('view engine', 'eta')
-  .set('views', `${paths.assets}/views`);
-
-// app middleware
-app
-  .use(helmet())
-  .use(middleware.httpLogger())
-  .use(cookieParser())
-  .use(compression())
-  .use(express.json())
-  .use(express.urlencoded({ extended: true }), hpp())
-  .use(express.static(paths.public))
-  .use(favicon(`${paths.public}/favicon.ico`))
-  .use(csurf({ cookie: true }), middleware.csrfToken());
-
-// app routes
-app.use('/', routers.home);
-
-// app error handlers
-app.use(middleware.notFound());
-app.use(middleware.errorHandler());
+import app from './index.js';
 
 // running express app server
 const PORT = parseInt(process.env.PORT, 10) || 5000;
-app
-  .listen(PORT, () => {
-    console.log(chalk.cyan('App server is up, listening PORT:'), PORT);
-  })
-  .on('error', err => {
-    console.error(chalk.red(err.message));
-    process.exit(1);
-  });
+const server = app.listen(PORT, () => {
+  console.log(chalk.cyan('App server is up, listening PORT:'), PORT);
+});
+
+server.on('error', err => {
+  switch (err.code) {
+    case 'EACCES':
+      console.error(chalk.red('Not enough privileges to run app server.'));
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(chalk.red(`${PORT} is already in use.`));
+      process.exit(1);
+      break;
+    default:
+      throw err;
+  }
+});
+
+// handle shutdown server gracefully
+const serverCloseHandler = exitCode => () => {
+  server.close();
+  process.exit(exitCode);
+};
+
+process.on('uncaughtException', serverCloseHandler(1));
+process.on('unhandledRejection', serverCloseHandler(1));
+
+['SIGINT', 'SIGTERM', 'SIGHUP'].forEach(signal => {
+  process.on(signal, serverCloseHandler(0));
+});
