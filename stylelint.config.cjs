430a3a6109f0694450dd2c53a4d06ab6aeb2db1a
borisding <boris.ding82@outlook.com>
2023-02-11 00:31:00 +0800

    stylelint amendments

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,7 +1,5 @@
 module.exports = {
-  plugins: ['stylelint-scss'],
-  extends: ['stylelint-config-sass-guidelines'],
-  customSyntax: 'postcss-scss',
+  extends: ['stylelint-config-standard', 'stylelint-config-standard-scss'],
   rules: {
     'selector-max-id': 1,
     'selector-class-pattern': null
