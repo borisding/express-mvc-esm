15db2448bd6f765e90bfccf3d92687bcf50d995c
Boris Ding <boris.ding82@outlook.com>
2024-09-01 20:52:48 +0800

    prettierignore, tweak

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -23,6 +23,6 @@ module.exports = [
       'no-global-assign': 'off',
       'prettier/prettier': ['error', prettierConfig]
     },
-    ignores: ['build', 'coverage', 'node_modules']
+    ignores: ['build/**/*', 'coverage/**/*']
   }
 ];
